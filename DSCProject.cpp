8a09473dc351bfefe9b6fdffca158981e4898185
Nabil Javeed Khaleel <75806477+nabeeljk345@users.noreply.github.com>
2020-12-12 15:31:33 -0500

    Add files via upload

---
+++ DSCProject.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+void main()
+int id1[9], id2[9], a, b, s1, s2;
+cout << "ID1:";
+cin >> id1[];
+cout << "ID2:";
+cin >> id2[];
+s1 = id1[8] + id2[8];
+s2 = id1[7] + id2[7];
+a = s1 % 5;
+b = (s2 % 5) + 5;
+cout << "Group code:" << "(a,b)";
